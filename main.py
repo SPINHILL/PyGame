3eb807e1f1a0b51a75fbc7b8f4d1c54075636106
brian.spinhill <brian.spinhill@mypc.com>
2024-04-26 12:26:04 +0100

    Added Invader class

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from animal import Animal; 
+from invader import Invader;
 
 # Example usage:
 my_animal = Animal("Lion", "Mammal")
 my_animal.display_info()
+
+my_invader = Invader("The Destroyer","Plasma Warrior",40,100)
+my_invader.display_info()

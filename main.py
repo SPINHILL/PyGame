27bf78dc98c54748a274aef7cd755e3be2f1121d
brian.spinhill <brian.spinhill@mypc.com>
2024-04-28 13:09:24 +0100

    created two invaders

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from animal import Animal; 
+
 from invader import Invader;
 
-# Example usage:
-my_animal = Animal("Lion", "Mammal")
-my_animal.display_info()
+
 
 my_invader = Invader("The Destroyer","Plasma Warrior",40,100)
+my_invader2 = Invader("heavy destroyer","Plasma Warrior",80,40)
 my_invader.display_info()
+my_invader.speed=80
+my_invader.health=60
+my_invader.display_info()
+my_invader2.display_info()
